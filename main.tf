7b27928c3f5fffa87148531425c24ffd49e90a05
sercom92 <srg.cg16@gmail.com>
2025-11-05 09:45:47 +0000

    Lab 1: Terraform local con Docker

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "docker_image" "nginx" {
 # Crear un contenedor a partir de la imagen
 resource "docker_container" "web" {
   name  = "web_server"
-  image = docker_image.nginx.latest
+  image = docker_image.nginx.name
   ports {
     internal = 80
     external = 8080
